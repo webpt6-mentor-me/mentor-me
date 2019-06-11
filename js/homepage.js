d3ed34b4fd249bfbf34f5f18baa42fedbaa58077
Benjamin Earley <benjaminearley@Benjamins-MacBook-Pro-2.local>
2019-06-10 21:03:25 -0400

    Page is fully responsive.

--- js/homepage.js
+++ js/homepage.js
@@ -1,10 +1,25 @@
+function openTab(event, tabName) {
+    let i, tabContent, tablink;
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    tablink = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablink.length; i++) {
+        tablink[i].className = tablink[i].className.replace("active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+
+}
+
+document.getElementById("defaultOpen").click();
 
 
 
 
 
 
-
-
-
-let buttons = document.querySelectorAll(".join-btn", (event));
+// let buttons = document.querySelectorAll(".join-btn", (event));
